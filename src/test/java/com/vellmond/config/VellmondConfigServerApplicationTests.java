ef2bd30397a4db833469268e06cb7c6fc78b49a1
Edgar Bautista <edgar.bautista@edu.uag.mx>
2024-08-08 22:42:43 -0600

    add first commit

---
+++ src/test/java/com/vellmond/config/VellmondConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vellmond.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VellmondConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
